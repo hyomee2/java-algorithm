ade7c79c3887802dad3f8d28e8a3d3327809d15e
Hyoungmi Kwon <108400640+hyomee2@users.noreply.github.com>
2025-01-29 02:07:49 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -9,7 +9,7 @@ public class Main {
         int[] nums = new int[m];
         int answer = 0;
         int left = 0;
-        int right = m;
+        int right = m - 1;
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < m; i++) {
@@ -17,14 +17,6 @@ public class Main {
         }
 
         Arrays.sort(nums);
-
-        // 고유한 재료의 값이 m 이하인 것만 고려하면 됨. right 인덱스 설정
-        for (int i = m - 1; i >= 0; i--) {
-            if (nums[i] <= n) {
-                right = i;
-                break;
-            }
-        }
 
         while (left < right) {
             if (nums[left] + nums[right] == n) {
