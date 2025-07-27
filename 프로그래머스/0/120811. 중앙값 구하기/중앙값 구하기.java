d8c88664fc43029ba77e84cb1852db6b459ea29f
Hyoungmi Kwon <108400640+hyomee2@users.noreply.github.com>
2025-07-27 23:03:41 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.54 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        
+        int length = array.length;
+        int answer = array[length / 2];
+        
+        return answer;
+    }
+}
