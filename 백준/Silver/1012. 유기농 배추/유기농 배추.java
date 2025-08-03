76c90c9e7d44f05f4c7c81f31cb54eff69dff1f1
Hyoungmi Kwon <108400640+hyomee2@users.noreply.github.com>
2025-08-03 22:12:12 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 16336 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int M, N, K, count, x, y;
+    static boolean[][] isVisited;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            // 지도 & 방문여부 만들기
+            map = new int[M][N];
+            isVisited = new boolean[M][N];
+            count = 0;
+
+            // 입력대로 배추 위치 표시하기
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+
+                map[x][y] = 1;
+            }
+
+            // 깊이우선탐색을 통해 최소의 배추흰지렁이 마리 수 출력
+            for (int j = 0; j < M; j++) {
+                for(int k = 0; k < N; k++) {
+                    if (!isVisited[j][k] && map[j][k] ==1) {
+                        dfs(j ,k);
+                        count++;
+                    }
+                }
+            }
+
+            sb.append(count).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int x, int y) {
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, -1, 0, 1};
+
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (!isValid(nx, ny)) continue;
+
+            isVisited[nx][ny] = true;
+            dfs(nx, ny);
+        }
+    }
+
+    public static boolean isValid(int x, int y) {
+        return (x >= 0 && x < M && y >= 0 && y < N && !isVisited[x][y] && map[x][y] == 1);
+    }
+}
