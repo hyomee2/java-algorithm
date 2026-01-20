557986a24c44d002241fb812dd445dbd1d94ed6d
Hyoungmi Kwon <108400640+hyomee2@users.noreply.github.com>
2026-01-20 19:38:17 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -5,61 +5,57 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();;
 
         Deque<Integer> queue = new ArrayDeque<>();
         String command;
         int num;
         Integer temp;
-        boolean isPush;
 
         int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             command = st.nextToken();
-            temp = null;
-            isPush = false;
 
             switch(command) {
                 case "push": {
                     num = Integer.parseInt(st.nextToken());
                     queue.add(num);
-                    isPush = true;
                     break;
                 }
 
                 case "pop": {
-                    temp = queue.poll();
-                    temp = (temp == null) ? -1 : temp;
+                    temp = queue.isEmpty() ? -1 : queue.poll();
+                    sb.append(temp).append('\n');
                     break;
                 }
 
                 case "size": {
                     temp = queue.size();
+                    sb.append(temp).append('\n');
                     break;
                 }
 
                 case "empty": {
                     temp = queue.isEmpty() ? 1 : 0;
+                    sb.append(temp).append('\n');
                     break;
                 }
 
                 case "front": {
-                    temp = queue.peekFirst();
-                    temp = (temp == null) ? -1 : temp;
+                    temp = (queue.isEmpty()) ? -1 : queue.peekFirst();
+                    sb.append(temp).append('\n');
                     break;
                 }
 
                 case "back": {
-                    temp = queue.peekLast();
-                    temp = (temp == null) ? -1 : temp;
+                    temp = (queue.isEmpty()) ? -1 : queue.peekLast();
+                    sb.append(temp).append('\n');
                     break;
                 }
             }
-
-            if (!isPush) {
-                System.out.println(temp);
-            }
         }
+
+        System.out.print(sb);
     }
 }
